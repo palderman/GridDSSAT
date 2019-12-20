cca1325f7d5776ae1997169fa510d3969a97078e
Phillip D. Alderman <phillip.alderman@okstate.edu>
2019-12-19 20:19:08 -0600

    Adding new function to generate benchmark scenarios.

--- R/run_all_benchmarks.R
+++ R/run_all_benchmarks.R
@@ -1,11 +1,5 @@
 #' @export
-run_all_benchmarks <- function(dssat_csm = '/DSSAT47/dscsm047',
-                               filex = paste0(dirname(dssat_csm),'/GridDSSAT/GridDSSAT_filex.nc'),
-                               wth_file = paste0(dirname(dssat_csm),'/Weather/idw.nc'),
-                               sol_file = paste0(dirname(dssat_csm),'/Soil/statsgo_new.nc'),
-                               varlist = 'HWAM',
-                               trt_start = 1,
-                               trt_end = 7208){
+run_all_benchmarks <- function(benchmark_scenarios){
 
     run_framework <- function(framework,...){
       arg_list <- deparse(list(...),width.cutoff=500L,nlines = 1)
@@ -24,39 +18,69 @@ run_all_benchmarks <- function(dssat_csm = '/DSSAT47/dscsm047',
     time_stamp <- Sys.time() %>%
       str_replace_all('[- ]+','_')
 
-    benchmark_scenarios <- expand.grid(framework = c('NetCDF','Text'),
-                                       ncores = seq(5,20,by=5)) %>%
-        dplyr::mutate(csm = str_c(dirname(dssat_csm),
-                                  '/dscsm047_',
-                                  if_else(framework == 'Text',
-                                          'std',
-                                          'mpi'))) %>%
-      dplyr::mutate(trt_start = trt_start,
-                    trt_end = trt_end,
-                    nc_out_file = str_c('results/',
-                                        time_stamp,'-',
-                                        framework,'-',
-                                        basename(csm),'-',
-                                        ncores,'cores','.nc'),
-                    varlist = varlist) %>%
-      dplyr::sample_frac()
-
-    saveRDS(benchmark_scenarios,str_c('results/',time_stamp,'-timings.rds'))
-
     timings <- benchmark_scenarios %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(timings = list(system.time({run_framework(framework,
-                                                              n_dssat = ncores,
-                                                              trt_start = trt_start,
-                                                              trt_end = trt_end,
-                                                              dssat_csm = csm,
-                                                              filex = filex,
-                                                              wth_file = wth_file,
-                                                              sol_file = sol_file,
-                                                              nc_out_file = nc_out_file)})))
+      dplyr::mutate(rowid = 1:n()) %>%
+      group_by(rowid) %>%
+      group_modify(~mutate(.,
+                           start_time = Sys.time(),
+                           run = run_framework(framework,
+                                               n_dssat = ncores,
+                                               trt_start = trt_start,
+                                               trt_end = trt_end,
+                                               dssat_csm = csm,
+                                               filex = filex,
+                                               wth_file = wth_file,
+                                               sol_file = sol_file,
+                                               nc_out_file = nc_out_file),
+                           end_time = Sys.time(),
+                           exec_time = difftime(end_time,start_time,units="secs"),
+                           exec_speed = 1/as.numeric(exec_time),
+                           exec_speed_per_core = exec_speed/ncores))
 
     saveRDS(timings,str_c('results/',time_stamp,'-timings.rds'))
 
     return(invisible())
+}
+
+#' @export
+generate_benchmarks_tbl <- function(nreps = 3,
+                                 dssat_csm = '/DSSAT47/dscsm047',
+                                 filex = paste0(dirname(dssat_csm),'/GridDSSAT/GridDSSAT_filex.nc'),
+                                 wth_file = paste0(dirname(dssat_csm),'/Weather/idw.nc'),
+                                 sol_file = paste0(dirname(dssat_csm),'/Soil/statsgo_new.nc'),
+                                 varlist = 'HWAM',
+                                 trt_start = 1,
+                                 trt_end = 7208){
+
+    time_stamp <- Sys.time() %>%
+      str_replace_all('[- ]+','_')
+
+    scenarios <- map(1:nreps,function(r){
+        rep_out <- expand.grid(framework = c('NetCDF','Text'),
+                               ncores = seq(5,20,by=5)) %>%
+          dplyr::mutate(csm = str_c(dirname(dssat_csm),
+                                    '/dscsm047_',
+                                    if_else(framework == 'Text',
+                                            'std',
+                                            'mpi'))) %>%
+          dplyr::mutate(trt_start = trt_start,
+                        trt_end = trt_end,
+                        nc_out_file = str_c('results/',
+                                            'rep',r,'-',
+                                            framework,'-',
+                                            basename(csm),'-',
+                                            ncores,'cores','.nc'),
+                        varlist = varlist) %>%
+          sample_frac()
+        return(rep_out)
+        }) %>%
+      bind_rows() %>%
+      ungroup() %>%
+      mutate(filex = filex,
+             wth_file = wth_file,
+             sol_file = sol_file)
+
+    return(scenarios)
+
 }
 
