dc79855da3f58a08de785d5cb4c3237fe82d88c0
Phillip D. Alderman <phillip.alderman@okstate.edu>
2020-01-03 08:21:39 -0600

    Removing --initsw_pct=100 from default dssat_args and adding other_dssat_args argument in run_netcdf_dssat()

--- R/run_netcdf_dssat.R
+++ R/run_netcdf_dssat.R
@@ -9,14 +9,16 @@ run_netcdf_dssat <- function(n_dssat=3,
                              wth_file = paste0(dirname(dssat_csm),'/Weather/idw.nc'),
                              sol_file = paste0(dirname(dssat_csm),'/Soil/statsgo_new.nc'),
                              gen_file = paste0(dirname(dssat_csm),'/Genotype/WHCER047.nc'),
-                           varlist = 'HWAM'){
-  
+                           varlist = 'HWAM',
+                           other_args = NULL,
+                           other_dssat_args = NULL){
+
   dssat_args <- c("--MPI",
                   str_c("--nc_wth=",wth_file),
                   str_c("--nc_soil=",sol_file),
                   str_c("--nc_filex=",filex),
                   str_c("--nc_gen=",gen_file),
-                  "--initsw_pct=100")
+                  other_dssat_args)
   run_mpi_call <- c("mpirun -n 1",
                     run_mpi_dssat,
                     str_c("--trt_start=",trt_start),
@@ -27,7 +29,8 @@ run_netcdf_dssat <- function(n_dssat=3,
                     str_c("--work_dir=",getwd()),
                     str_c("--nc_filex=",filex),
                     str_c("--nc_out=",nc_out_file),
-                    str_c("--dssat_args=",str_c(dssat_args, collapse = ','))) %>%
+                    str_c("--dssat_args=",str_c(dssat_args, collapse = ',')),
+                    other_args) %>%
     str_c(collapse = ' ')
 
   system(run_mpi_call)
